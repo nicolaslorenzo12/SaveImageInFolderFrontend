4f66c7828c0b2900810257e7ae899d831a7c1a81
niselo <nicolas.lorenzo@student.kdg.be>
2024-06-11 12:43:43 +0200

    I can take a picture when pressing usb button

--- src/App.js
+++ src/App.js
@@ -13,3 +13,46 @@ const App = () => {
   };
   
   export default App;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import usbDetect from 'usb-detection';
+
+// const App = () => {
+//   const [buttonPressed, setButtonPressed] = useState(false);
+
+//   useEffect(() => {
+//     // Start USB device detection
+//     usbDetect.startMonitoring();
+
+//     // Add event listener for USB device connection
+//     usbDetect.on('add', (device) => {
+//       // Check if the connected device is your USB button
+//       if (device.vendorId ===  0x5131 && device.productId === 0x2019) {
+//         // Handle USB button connection
+//         console.log('USB button connected');
+//         // Subscribe to button press events
+//         device.on('data', (data) => {
+//           // Check if the received data indicates a button press
+//           if (data[0] === 1) {
+//             setButtonPressed(true);
+//           }
+//         });
+//       }
+//     });
+
+//     // Cleanup on unmount
+//     return () => {
+//       usbDetect.stopMonitoring();
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       {buttonPressed ? <p>Button pressed</p> : <p>Button not pressed</p>}
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/WebcamStream.js
+++ src/WebcamStream.js
@@ -1,3 +1,127 @@
+// import React, { useState, useRef, useEffect, useCallback } from 'react';
+// import WebcamCaptureForm from './WebcamCaptureForm';
+// import { sendImageToBackend } from './SendPicture';
+// import { fetchIfFolderExists } from './api/FetchIfFolderExists';
+// import { fetchAllFolders } from './api/FetchAllFolders';
+
+// const WebcamCapturePicture = () => {
+//   const [selectedFolder, setSelectedFolder] = useState('');
+//   const [folders, setFolders] = useState([]);
+//   const [folderIsNull, setFolderIsNull] = useState(false)
+//   const [showImage, setShowImage] = useState(false);
+//   const webcamRef = useRef(null);
+//   const [imgSrc, setImgSrc] = useState('');
+//   const [countdown, setCountdown] = useState(0);
+//   const [isCountingDown, setIsCountingDown] = useState(false)
+
+//   useEffect(() => {
+//     const handleKeyDown = (event) => {
+//       if (event.key === 'F2') {
+//        startCountdown();
+//         event.preventDefault(); // Prevent the default action (opening help in most browsers)
+//       }
+//     };
+
+//     window.addEventListener('keydown', handleKeyDown);
+
+//     // Cleanup the event listener on component unmount
+//     return () => {
+//       window.removeEventListener('keydown', handleKeyDown);
+//     };
+//   }, []);
+
+//   const handleFolderChange = (event) => {
+//     setSelectedFolder(event.target.value);
+//   };
+
+//   const capturePicture = useCallback(async () => {
+//     const imageSrc = await webcamRef.current.getScreenshot();
+//     setImgSrc(imageSrc);
+//   }, [webcamRef]);
+
+//   useEffect(() => {
+//     if (imgSrc) {
+//       saveImageAndShowPicture();
+//     }
+//   }, [imgSrc]);
+
+//   const handleShowImage = () =>{
+//     setShowImage(true);
+//     setTimeout(() => setShowImage(false), 5000);
+//   }
+
+//   const saveImageAndShowPicture = async () => {
+//     try {
+//       const exists = await fetchIfFolderExists(selectedFolder);
+//       await sendImageToBackend(selectedFolder, imgSrc);
+  
+//       if (!exists) {
+//         setFolders(prevFolders => [...prevFolders, selectedFolder]);       
+//       }
+//       setSelectedFolder('');
+//       setFolderIsNull(false);
+//       handleShowImage();
+//     } catch (error) {
+//       console.error('Error checking or adding folder:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     const fetchFolders = async () => {
+//       try {
+//         const data = await fetchAllFolders();
+//         setFolders(data);
+//       } catch (error) {
+//         console.error('Error fetching folders:', error);
+//       }
+//     };
+
+//     fetchFolders();
+//   }, []); 
+
+//   const startCountdown = () => {
+//     if(selectedFolder !== ''){
+//       setFolderIsNull(false);
+//       setCountdown(5);
+//       setIsCountingDown(true);
+//     }
+//     else{
+//       setFolderIsNull(true);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (isCountingDown && countdown > 0) {
+//       const timer = setTimeout(() => {
+//         setCountdown(prevCountdown => prevCountdown - 1);
+//       }, 1000);
+//       return () => clearTimeout(timer);
+//     } else if (isCountingDown && countdown === 0) {
+//       capturePicture();
+//       setIsCountingDown(false);
+//     }
+//   }, [isCountingDown, countdown, capturePicture]);
+
+//   return (
+//     <WebcamCaptureForm
+//       selectedFolder={selectedFolder}
+//       folderIsNull={folderIsNull}
+//       handleFolderChange={handleFolderChange}
+//       folders={folders}
+//       startCountdown={startCountdown}
+//       isCountingDown={isCountingDown}
+//       countdown={countdown}
+//       webcamRef={webcamRef}
+//       showImage={showImage}
+//       imgSrc={imgSrc}
+//     />
+//   );
+// }
+// export default WebcamCapturePicture;
+
+
+
+
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import WebcamCaptureForm from './WebcamCaptureForm';
 import { sendImageToBackend } from './SendPicture';
@@ -7,12 +131,35 @@ import { fetchAllFolders } from './api/FetchAllFolders';
 const WebcamCapturePicture = () => {
   const [selectedFolder, setSelectedFolder] = useState('');
   const [folders, setFolders] = useState([]);
-  const [folderIsNull, setFolderIsNull] = useState(false)
+  const [folderIsNull, setFolderIsNull] = useState(false);
   const [showImage, setShowImage] = useState(false);
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState('');
   const [countdown, setCountdown] = useState(0);
-  const [isCountingDown, setIsCountingDown] = useState(false)
+  const [isCountingDown, setIsCountingDown] = useState(false);
+
+  // Ref to store the latest selectedFolder value
+  const selectedFolderRef = useRef(selectedFolder);
+
+  useEffect(() => {
+    selectedFolderRef.current = selectedFolder;
+  }, [selectedFolder]);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'F2') {
+        startCountdown();
+        event.preventDefault(); // Prevent the default action (opening help in most browsers)
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   const handleFolderChange = (event) => {
     setSelectedFolder(event.target.value);
@@ -29,10 +176,10 @@ const WebcamCapturePicture = () => {
     }
   }, [imgSrc]);
 
-  const handleShowImage = () =>{
+  const handleShowImage = () => {
     setShowImage(true);
     setTimeout(() => setShowImage(false), 5000);
-  }
+  };
 
   const saveImageAndShowPicture = async () => {
     try {
@@ -40,7 +187,7 @@ const WebcamCapturePicture = () => {
       await sendImageToBackend(selectedFolder, imgSrc);
   
       if (!exists) {
-        setFolders(prevFolders => [...prevFolders, selectedFolder]);       
+        setFolders((prevFolders) => [...prevFolders, selectedFolder]);       
       }
       setSelectedFolder('');
       setFolderIsNull(false);
@@ -64,12 +211,11 @@ const WebcamCapturePicture = () => {
   }, []); 
 
   const startCountdown = () => {
-    if(selectedFolder !== ''){
+    if (selectedFolderRef.current !== '') {
       setFolderIsNull(false);
       setCountdown(5);
       setIsCountingDown(true);
-    }
-    else{
+    } else {
       setFolderIsNull(true);
     }
   };
@@ -77,7 +223,7 @@ const WebcamCapturePicture = () => {
   useEffect(() => {
     if (isCountingDown && countdown > 0) {
       const timer = setTimeout(() => {
-        setCountdown(prevCountdown => prevCountdown - 1);
+        setCountdown((prevCountdown) => prevCountdown - 1);
       }, 1000);
       return () => clearTimeout(timer);
     } else if (isCountingDown && countdown === 0) {
@@ -100,5 +246,6 @@ const WebcamCapturePicture = () => {
       imgSrc={imgSrc}
     />
   );
-}
+};
+
 export default WebcamCapturePicture;
