14e73434f1836eb537068178429cdea4fdb66624
niselo <nicolas.lorenzo@student.kdg.be>
2024-06-18 10:22:35 +0200

    i can take a picture and show it for five seconds and then show the stream again

--- src/components/WebcamCaptureForm.js
+++ src/components/WebcamCaptureForm.js
@@ -1,3 +1,67 @@
+// import React from 'react';
+// import Webcam from "react-webcam"
+// import '../style/style.css'
+
+// const WebcamCaptureForm = ({
+//   selectedFolder,
+//   folderIsNull,
+//   handleFolderChange,
+//   folders,
+//   startCountdownWhenClickingButtonIfSelectedFolderNotNull,
+//   isCountingDown,
+//   countdown,
+//   webcamRef,
+//   showImage,
+//   imgSrc,
+//   showStream,
+//   showButtons,
+//   setImageToBeSaved,
+//   toggleShowImageShowStreamAndShowButtons,
+//   showSavedImageMessage,
+// }) => {
+//   return (
+//     <div>
+//       <h1>Webcam Capture</h1>
+//       <label htmlFor="dropdown">Choose existing folder:</label>
+//       <div>
+//         <input
+//           list="options"
+//           id="dropdown"
+//           value={selectedFolder}
+//           onChange={handleFolderChange}
+//           autoComplete="off"
+//         />
+//         {folderIsNull && <p id="folder-is-null">Folder cannot be null</p>}
+//       </div>
+//       <datalist id="options">
+//         {folders.map((folder, index) => (
+//           <option key={index} value={folder} />
+//         ))}
+//       </datalist>
+//       <button onClick={startCountdownWhenClickingButtonIfSelectedFolderNotNull}>Capture photo</button>
+//       {isCountingDown && <div id='counter'>{countdown}</div>}
+//       <div>
+//         {showStream && <Webcam ref={webcamRef} />}
+//       </div>
+//       {showImage && (
+//         <div>
+//           <img src={imgSrc} alt="" />
+//         </div>
+//       )}
+//       <div id="buttons-to-save-or-discard-image">
+//         {showButtons && <button onClick={setImageToBeSaved} id="save-image-button">Save image</button>}
+//         {showButtons && <button onClick={toggleShowImageShowStreamAndShowButtons} id="discard-image-button">Discard image</button>}
+//       </div>
+//       {showSavedImageMessage && <p id="image-saved-message">Image was successfully saved!</p>}
+//     </div>
+//   );
+// };
+
+// export default WebcamCaptureForm;
+
+
+
+
 import React from 'react';
 import Webcam from "react-webcam"
 import '../style/style.css'
@@ -14,10 +78,6 @@ const WebcamCaptureForm = ({
   showImage,
   imgSrc,
   showStream,
-  showButtons,
-  setImageToBeSaved,
-  toggleShowImageShowStreamAndShowButtons,
-  showSavedImageMessage,
 }) => {
   return (
     <div>
@@ -48,11 +108,6 @@ const WebcamCaptureForm = ({
           <img src={imgSrc} alt="" />
         </div>
       )}
-      <div id="buttons-to-save-or-discard-image">
-        {showButtons && <button onClick={setImageToBeSaved} id="save-image-button">Save image</button>}
-        {showButtons && <button onClick={toggleShowImageShowStreamAndShowButtons} id="discard-image-button">Discard image</button>}
-      </div>
-      {showSavedImageMessage && <p id="image-saved-message">Image was successfully saved!</p>}
     </div>
   );
 };
