0e85daa35e95eed0eb48bc1a58cf9ce749a04510
niselo <nicolas.lorenzo@student.kdg.be>
2024-06-19 16:59:57 +0200

    style is much better

--- src/components/WebcamCaptureForm.js
+++ src/components/WebcamCaptureForm.js
@@ -1,6 +1,75 @@
+// import React from 'react';
+// import Webcam from "react-webcam"
+// import '../style/style.css'
+
+// const WebcamCaptureForm = ({
+//   selectedFolder,
+//   folderIsNull,
+//   handleFolderChange,
+//   folders,
+//   startCountdownWhenClickingButtonIfSelectedFolderNotNull,
+//   isCountingDown,
+//   countdown,
+//   webcamRef,
+//   showImage,
+//   imgSrc,
+//   showStream,
+//   showLastFiveImages,
+//   lastFiveImages,
+// }) => {
+//   return (
+//     <div>
+//       <h1>Webcam Capture</h1>
+//       <label htmlFor="dropdown">Choose existing folder:</label>
+//       <div>
+//         <input
+//           list="options"
+//           id="dropdown"
+//           value={selectedFolder}
+//           onChange={handleFolderChange}
+//           autoComplete="off"
+//         />
+//         {folderIsNull && <p id="folder-is-null">Folder cannot be null</p>}
+//       </div>
+//       <datalist id="options">
+//         {folders.map((folder, index) => (
+//           <option key={index} value={folder} />
+//         ))}
+//       </datalist>
+//       <button onClick={startCountdownWhenClickingButtonIfSelectedFolderNotNull}>Capture photo</button>
+//       {isCountingDown && <div id='counter'>{countdown}</div>}
+//       <div>
+//         {showStream && <Webcam ref={webcamRef} />}
+//       </div>
+//       {showImage && (
+//         <div>
+//           <img src={imgSrc} alt="" />
+//         </div>
+//       )}
+
+//       <div id="last-five-images">
+//         {showLastFiveImages && lastFiveImages.map((image, index) => (
+//           <img key={index} src={`data:image/jpeg;base64,${image.data}`} alt={`Captured ${index}`} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WebcamCaptureForm;
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
-import Webcam from "react-webcam"
-import '../style/style.css'
+import Webcam from "react-webcam";
+import '../style/style.css';
 
 const WebcamCaptureForm = ({
   selectedFolder,
@@ -19,9 +88,9 @@ const WebcamCaptureForm = ({
 }) => {
   return (
     <div>
-      <h1>Webcam Capture</h1>
-      <label htmlFor="dropdown">Choose existing folder:</label>
-      <div>
+      <h1>Photo-Book</h1>
+      <label htmlFor="dropdown">Choose folder:</label>
+      <div id="dropdown-container">
         <input
           list="options"
           id="dropdown"
@@ -29,28 +98,32 @@ const WebcamCaptureForm = ({
           onChange={handleFolderChange}
           autoComplete="off"
         />
-        {folderIsNull && <p id="folder-is-null">Folder cannot be null</p>}
       </div>
+      {folderIsNull && <p id="folder-is-null">Folder cannot be null</p>}
       <datalist id="options">
         {folders.map((folder, index) => (
           <option key={index} value={folder} />
         ))}
       </datalist>
-      <button onClick={startCountdownWhenClickingButtonIfSelectedFolderNotNull}>Capture photo</button>
+      <div id="button-container">
+        <button onClick={startCountdownWhenClickingButtonIfSelectedFolderNotNull}>Capture photo</button>
+      </div>
       {isCountingDown && <div id='counter'>{countdown}</div>}
-      <div>
+      <div id="webcam-container">
         {showStream && <Webcam ref={webcamRef} />}
       </div>
       {showImage && (
-        <div>
-          <img src={imgSrc} alt="" />
+        <div id="captured-image">
+          <img src={imgSrc} alt="Captured" />
         </div>
       )}
 
-      <div id="last-five-images">
-        {showLastFiveImages && lastFiveImages.map((image, index) => (
-          <img key={index} src={`data:image/jpeg;base64,${image.data}`} alt={`Captured ${index}`} />
-        ))}
+      <div id="last-five-images-container">
+        <div id="last-five-images">
+          {showLastFiveImages && lastFiveImages.map((image, index) => (
+            <img key={index} src={`data:image/jpeg;base64,${image.data}`} alt={`Captured ${index}`} />
+          ))}
+        </div>
       </div>
     </div>
   );
